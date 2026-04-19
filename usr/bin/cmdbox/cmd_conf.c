3cca543426766e59a18ab0837b86ff69828e3f13
Champ Yen <champ.yen@gmail.com>
2026-04-19 20:00:33 +0800

    Port dd, hexdump, and od to cmdbox
    - Ported 'dd' utility from RetroBSD to usr/bin/dd; replaced legacy sbrk() calls
      with standard malloc() for better compatibility.
    - Ported 'hexdump' utility from LiteBSD to usr/bin/hexdump, providing both
      'hexdump' and 'od' functionality in cmdbox.
    - Resolved BSD-to-Prex compatibility issues by mapping missing quad_t types
      and renaming local err() definitions to avoid libc conflicts.
    - Registered all new commands in cmdbox and enabled them in the default
      qemu-virt target configuration.
    - Verified system stability and SDK generation with parallel build (make -j6).

--- usr/bin/cmdbox/cmd_conf.c
+++ usr/bin/cmdbox/cmd_conf.c
@@ -35,10 +35,12 @@ extern int cat_main(int argc, char* argv[]);
 extern int clear_main(int argc, char* argv[]);
 extern int cp_main(int argc, char* argv[]);
 extern int date_main(int argc, char* argv[]);
+extern int dd_main(int argc, char* argv[]);
 extern int dmesg_main(int argc, char* argv[]);
 extern int echo_main(int argc, char* argv[]);
 extern int free_main(int argc, char* argv[]);
 extern int head_main(int argc, char* argv[]);
+extern int hexdump_main(int argc, char* argv[]);
 extern int hostname_main(int argc, char* argv[]);
 extern int kill_main(int argc, char* argv[]);
 extern int ls_main(int argc, char* argv[]);
@@ -83,6 +85,9 @@ const struct cmdentry builtin_cmds[] = {
 #ifdef CONFIG_CMD_DATE
     {"date", date_main},
 #endif
+#ifdef CONFIG_CMD_DD
+    {"dd", dd_main},
+#endif
 #ifdef CONFIG_CMD_DMESG
     {"dmesg", dmesg_main},
 #endif
@@ -94,6 +99,10 @@ const struct cmdentry builtin_cmds[] = {
 #endif
 #ifdef CONFIG_CMD_HEAD
     {"head", head_main},
+#endif
+#ifdef CONFIG_CMD_HEXDUMP
+    {"hexdump", hexdump_main},
+    {"od", hexdump_main},
 #endif
     {"help", help_main},
 #ifdef CONFIG_CMD_HOSTNAME
